9a4e9efdc721496fbaf3cd16ecaf8effd4d099e3
Pravin Tumsare <pravintumsare@gmail.com>
2020-11-18 09:02:37 +0000

    added deployment.yaml into kustomization

--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -11,5 +11,6 @@ public class WelcomeControllerTest {
     public void itSaysHello() throws Exception {
         WelcomeController controller = new WelcomeController("hello from kubernetes");
         assertThat(controller.sayHello()).isEqualTo("hello from kubernetes");
+        //build commit
     }
 }
